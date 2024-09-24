073a7344bd11d8f7b4d9465bf33afb4e6db8b030
Akshar Patel <akshar305@gmail.com>
2024-09-24 18:14:45 -0400

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from src.RenalDiseaseClassification import logger
+from RenalDiseaseClassification import logger
+from RenalDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Welcome to Renal Disease Classification Project')
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
